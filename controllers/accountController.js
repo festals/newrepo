43996efe7315da5c8fbdf2b56f959d2e611c3f40
festals <festals90@gmail.com>
2025-03-26 18:15:39 +0100

    server side datat validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -9,7 +9,7 @@ async function buildLogin(req, res, next) {
     res.render("account/login", {
         title: "Login",
         nav,
-        errors: null
+        // errors: null,
     })
 }
 
@@ -21,7 +21,7 @@ async function buildRegister(req, res, next) {
     res.render("account/register", {
       title: "Register",
       nav,
-      errors: null
+      errors: null,
     })
 }
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,10 +3,14 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require("../utilities/account-validation")
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
-
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+// Process the registration data
+router.post("/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount));
 
 module.exports = router;
