9430644b6fd7ca5ba6afe05b5e03bb8333431809
festals <festals90@gmail.com>
2025-03-31 16:11:06 +0200

    assignment w04

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,11 +3,32 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/")
+const validate = require("../utilities/inventory-validation")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 // Route to build details by inventory view
 router.get("/detail/:inventoryId", utilities.handleErrors(invController.getInventoryDetail));
+// Route to build management form by inventory view
+router.get("/management", utilities.handleErrors(invController.buildManagementView))
+// Route to build add classification form by inventory view
+router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification))
 
+router.post(
+    "/add-classification",
+    validate.classificationRules(),
+    validate.checkClassData,
+    utilities.handleErrors(invController.addClassification)
+)
+
+// Route to build add inventory form by inventory view
+router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventoryView))
+
+router.post(
+    "/add-inventory",
+    validate.invRules(),
+    validate.checkInvData,
+    utilities.handleErrors(invController.addInventory)
+)
 
 module.exports = router;
