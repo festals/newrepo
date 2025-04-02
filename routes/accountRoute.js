751c2a293227d26aa7bd9e44da9bd26ae4525a9a
festals <festals90@gmail.com>
2025-04-02 16:03:53 +0200

    w05 learning act :Authentication and Authorization

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,24 +5,22 @@ const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
 const regValidate = require("../utilities/account-validation")
 
+router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.buildAccountView));
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 // Process the registration data
 router.post("/register",
   regValidate.registationRules(),
   regValidate.checkRegData,
-  utilities.handleErrors(accountController.registerAccount));
+  utilities.handleErrors(accountController.registerAccount)
+);
 
-// Process the login attempt
+// Process the login request
 router.post(
   "/login",
   regValidate.loginRules(),
   regValidate.checkLoginData,
-  utilities.handleErrors(
-    (req, res) => {
-      res.status(200).send('login process')
-    }
-  )
+  utilities.handleErrors(accountController.accountLogin)
 );
 
 module.exports = router;
